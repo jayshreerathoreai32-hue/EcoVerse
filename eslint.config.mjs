6282f26135fcdf4bc8bc236ce8ef2f98c83fc859
Bhagyasri <bhagyadhakshina2k9@gmail.com>
2026-06-07 19:29:34 +0530

    fix: include Next.js TypeScript ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import unusedImports from "eslint-plugin-unused-imports";
 
 export default [
   ...nextVitals,
+  ...nextTypescript,
   {
     plugins: {
       "unused-imports": unusedImports,
